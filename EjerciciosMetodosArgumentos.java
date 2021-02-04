e7f2ff4437e6acb3c0492f865f2e4f2ae8ed6a10
adolfopalma <adolfo.palmagr@gmail.com>
2021-02-04 20:14:06 +0100

    Ejercicios para practicar los metodos con arg

---
+++ EjerciciosMetodosArgumentos.java
@@ -0,0 +1,17 @@
+
+//Crea un metodo que te pida tu nombre y lo introduzcas por teclado y lanza un saludo personalizado para ese nombre.
+
+public class EjerciciosMetodosArgumentos{
+
+
+    public static void main(String[] args){
+
+    }
+
+
+
+
+
+
+    
+}
